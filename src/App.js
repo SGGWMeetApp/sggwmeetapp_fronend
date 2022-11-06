56b9a88d07ee9c4c232542ec710c491a48342941
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2022-11-06 14:19:55 +0100

    add login to route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage/HomePage.js";
+import LogInPage from "./LogInPage/LogInPage.js";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LogInPage/>}/>
       </Routes>
     </BrowserRouter>
   );
