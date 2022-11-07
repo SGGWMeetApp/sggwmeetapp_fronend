2371ae7a24e767d6fa48594d429d661541d0c68d
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2022-11-07 23:07:51 +0100

    add new Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage/HomePage.js";
 import LogInPage from "./LogInPage/LogInPage.js";
+import RegisterPage from "./RegisterPage/RegisterPage.js"
+import MainPage from "./MainPage/MainPage.js"
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LogInPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/profile" element={<MainPage/>}/>
       </Routes>
     </BrowserRouter>
   );
