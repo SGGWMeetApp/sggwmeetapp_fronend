8241bcabbecc5782e70beddbc47add35ddf79426
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2022-11-10 23:28:07 +0100

    add functions form modal item

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -4,11 +4,26 @@ import style from "./MainPage.module.css";
 import Navigation from "./MainPageNavi.js";
 import Foto from "../Assets/section2_photo.jpg";
 import { NavLink } from "react-router-dom";
+import UserProfile from "./ModalWindows/UserProfile.js";
+import ModalWindow from "./ModalWindows/Modal";
 class MainPage extends React.Component {
+  state = {
+    openModal: false,
+  };
+  OpenModal = () => {
+    this.setState({
+      openModal: true,
+    });
+  };
+  CloseModal = () => {
+    this.setState({
+      openModal: false,
+    });
+  };
   render() {
     return (
       <div className={style.MainPage}>
-        <Navigation />
+        <Navigation OpenModal={this.OpenModal} />
         <div className={style.PlacesContainer}>
           <div className={style.SectionHeader}>
             <p className={style.ObjectNumber}>68 miejsc w okolicy</p>
@@ -26,29 +41,36 @@ class MainPage extends React.Component {
             <ul className={style.ObjectList}>
               <li>
                 <NavLink to="/profile/object">
-                <div className={style.ListElemnet}>
-                  <img src={Foto} alt="" />
-                  <div className={style.ObjectDescribe}>
-                    <p className={style.ObjectName}>Restauracja Nova</p>
-                    <p className={style.ObjectOpinions}>
-                      <Icon
-                        icon="majesticons:percent"
-                        color="#857E7B"
-                        width="16"
-                        height="16"
-                      />
-                      78 [6]
-                    </p>
-                    <p className={style.ObjecAddress}>450m Potocka 7a</p>
-                    <p className={style.ObjectType}>Restauracja</p>
+                  <div className={style.ListElemnet}>
+                    <img src={Foto} alt="" />
+                    <div className={style.ObjectDescribe}>
+                      <p className={style.ObjectName}>Restauracja Nova</p>
+                      <p className={style.ObjectOpinions}>
+                        <Icon
+                          icon="majesticons:percent"
+                          color="#857E7B"
+                          width="16"
+                          height="16"
+                        />
+                        78 [6]
+                      </p>
+                      <p className={style.ObjecAddress}>450m Potocka 7a</p>
+                      <p className={style.ObjectType}>Restauracja</p>
+                    </div>
                   </div>
-                </div>
                 </NavLink>
               </li>
             </ul>
           </div>
         </div>
-        <div className={style.MapContainer}></div>
+        <div className={style.MapContainer}>
+          <ModalWindow
+            openModal={this.state.openModal}
+            onClose={!this.state.openModal}
+          >
+            <UserProfile CloseModal={this.CloseModal} />
+          </ModalWindow>
+        </div>
       </div>
     );
   }
