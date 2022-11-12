5e9f903822dfd5e443a9cdb371d54efd4b39dc05
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2022-11-12 19:12:14 +0100

    changed show TableRow

--- src/GroupsPage/TableRow.js
+++ src/GroupsPage/TableRow.js
@@ -2,6 +2,8 @@ import React from "react";
 import style from "./GroupsPage.module.css";
 import { Icon } from "@iconify/react";
 import Menu from "./ContextMenu.js";
+import MenuEvents from "../EventsPage/EventsContextMenu.js";
+import MenuMembers from "./MembersContextMenu";
 class TableRow extends React.Component {
   state = {
     visible: false,
@@ -11,21 +13,34 @@ class TableRow extends React.Component {
   };
   render() {
     return (
-        <td>
-          {!this.state.visible && (
-            <button onClick={this.handleShow} className={style.MoreBtn}>
-              <Icon
-                icon="akar-icons:more-vertical"
-                color="rgba(0, 0, 0, 0.54)"
-                width="20"
-                height="20"
-              />
-            </button>
-          )}
-          {this.state.visible && (
-            <Menu visible={this.state.visible} handleShow={this.handleShow} />
-          )}
-        </td>
+      <td>
+        {!this.state.visible && (
+          <button onClick={this.handleShow} className={style.MoreBtn}>
+            <Icon
+              icon="akar-icons:more-vertical"
+              color="rgba(0, 0, 0, 0.54)"
+              width="20"
+              height="20"
+            />
+          </button>
+        )}
+        {this.state.visible && this.props.id === "groups" && (
+          <Menu visible={this.state.visible} handleShow={this.handleShow} />
+        )}
+        {this.state.visible && this.props.id === "events" && (
+          <MenuEvents
+            visible={this.state.visible}
+            handleShow={this.handleShow}
+          />
+        )}
+        {this.state.visible && this.props.id === "members" && (
+          <MenuMembers
+            visible={this.state.visible}
+            handleShow={this.handleShow}
+            OpenModal={this.props.OpenModal}
+          />
+        )}
+      </td>
     );
   }
 }
