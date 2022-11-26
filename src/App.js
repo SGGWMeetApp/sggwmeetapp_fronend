bac1bef0bc7603d3aff7e33e65f097fbb81d42c4
Moonikaa123 <98284081+Moonikaa123@users.noreply.github.com>
2022-11-26 09:36:49 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./HomePage/HomePage.js";
-import LogInPage from "./LogInPage/LogInPage.js";
-import RegisterPage from "./RegisterPage/RegisterPage.js";
-import MainPage from "./MainPage/MainPage.js";
-import GroupsPage from "./GroupsPage/GroupsPage.js";
-import ObjectPage from "./ObjectPage/ObjectMainPage.js";
-import EventsPage from "./EventsPage/EventsPage.js";
-import GroupMemebers from "./GroupsPage/GroupMembers.js"
-import GroupEvents from "./GroupsPage/GroupEvent.js";
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import HomePage from './HomePage/HomePage.js';
+import LogInPage from './LogInPage/LogInPage.js';
+import RegisterPage from './RegisterPage/RegisterPage.js';
+import MainPage from './MainPage/MainPage.js';
+import GroupsPage from './GroupsPage/GroupsPage.js';
+import ObjectPage from './ObjectPage/ObjectMainPage.js';
+import EventsPage from './EventsPage/EventsPage.js';
+import GroupMemebers from './GroupsPage/GroupMembers.js';
+import GroupEvents from './GroupsPage/GroupEvent.js';
+import { useAuthContext } from './context/AuthContext';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<LogInPage/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/profile" element={<MainPage/>}/>
-        <Route path="/profile/groups" element={<GroupsPage/>}/>
-        <Route path="/profile/object" element={<ObjectPage/>}/>
-        <Route path="/profile/events" element={<EventsPage/>}/>
-        <Route path="/profile/groups/id/members" element={<GroupMemebers/>}/>
-        <Route path="/profile/groups/id/events" element={<GroupEvents/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+   const { user } = useAuthContext();
+
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route
+               path="/"
+               element={!user ? <HomePage /> : <Navigate to="/profile" />}
+            />
+            <Route
+               path="/login"
+               element={!user ? <LogInPage /> : <Navigate to="/profile" />}
+            />
+            <Route
+               path="/register"
+               element={!user ? <RegisterPage /> : <Navigate to="/profile" />}
+            />
+            <Route
+               path="/profile"
+               element={user ? <MainPage /> : <Navigate to="/" />}
+            />
+            <Route
+               path="/profile/groups"
+               element={user ? <GroupsPage /> : <Navigate to="/" />}
+            />
+            <Route
+               path="/profile/object"
+               element={user ? <ObjectPage /> : <Navigate to="/" />}
+            />
+            <Route
+               path="/profile/events"
+               element={user ? <EventsPage /> : <Navigate to="/" />}
+            />
+            <Route
+               path="/profile/groups/id/members"
+               element={user ? <GroupMemebers /> : <Navigate to="/" />}
+            />
+            <Route
+               path="/profile/groups/id/events"
+               element={user ? <GroupEvents /> : <Navigate to="/" />}
+            />
+         </Routes>
+      </BrowserRouter>
+   );
 };
 
 export default App;
