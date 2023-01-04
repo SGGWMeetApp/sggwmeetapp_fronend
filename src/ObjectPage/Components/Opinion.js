4e53e7eaaad63c8f562b144afe9e650a5153d4d9
Mateusz <63258155+MateuszZelaszczyk@users.noreply.github.com>
2023-01-04 19:26:08 +0100

    changed view for opinions view in object details

--- src/ObjectPage/Components/Opinion.js
+++ src/ObjectPage/Components/Opinion.js
@@ -1,20 +1,26 @@
 import React from "react";
 import style from "./Components.module.css";
 import { Icon } from "@iconify/react";
-import Foto from "../../Assets/section2_photo.jpg"
+import UserAvatar from "../../Assets/User-avatar.svg";
 const Opinions = (props) => {
+  const rating = props.rating;
+
   return (
     <div className={style.OpinionContainer}>
       <div className={style.OpinionsHeader}>
-        <p>
-          <Icon
-            icon="majesticons:percent"
-            color="rgba(0, 0, 0, 0.87)"
-            width="24"
-            height="24"
-          />
-          66 z 3 opini
-        </p>
+        {rating.reviews.length !== 0 ? (
+          <div>
+            <p style={{ marginBottom: "0px" }}>
+              {rating.positivePercent}% poleca to miejsce
+            </p>
+            <p style={{ fontSize: "18px", marginTop: "0px" }}>
+              {" "}
+              {rating.reviews.length} opini
+            </p>
+          </div>
+        ) : (
+          <p>To miejsce nie otrzymało jeszcze opini</p>
+        )}
         <button className={style.AddOpinion}>
           <Icon
             icon="ant-design:plus-outlined"
@@ -26,43 +32,81 @@ const Opinions = (props) => {
         </button>
       </div>
       <div className={style.Opinions}>
-        <div className={style.Opinion}>
-          <div className={style.OpinionHeader}>
-            <img className={style.Avatar} src={Foto} alt="Person" />
-            <div className={style.UserInfo}>
-              <p className={style.UserName}>Jan Nowak</p>
-              <p className={style.Date}>Czerwiec 2022</p>
-            </div>
-            <div className={style.OpinionRate}>
-              <Icon icon="ant-design:like-filled" color="#85c9b9" width="30" />
-            </div>
-            <div className={style.ReviewRating}>
-              <div className={style.Positive}>
-                <Icon
-                  icon="ant-design:like-filled"
-                  color="rgba(18, 44, 52, 0.5)"
-                  width="20"
-                  height="20"
-                />
-                <p className={style.PositiveNumber}>2</p>
+        {rating.reviews.length !== 0 ? (
+          rating.reviews.map((review) => (
+            <div className={style.Opinion}>
+              <div key={review.id} className={style.OpinionHeader}>
+                {review.author.avatarUrl ? (
+                  <img
+                    className={style.Avatar}
+                    src={review.author.avatarUrl}
+                    alt="Avatar"
+                  />
+                ) : (
+                  <img
+                    className={style.Avatar}
+                    src={UserAvatar}
+                    alt="Avatar"
+                  />
+                )}
+
+                <div className={style.UserInfo}>
+                  <p className={style.UserName}>
+                    {review.author.firstName} {review.author.lastName}
+                  </p>
+                  <p className={style.Date}>
+                    {review.publicationDate.slice(0, 10)}
+                  </p>
+                </div>
+                <div className={style.OpinionRate}>
+                  {review.isPositive ? (
+                    <Icon
+                      icon="ant-design:like-filled"
+                      color="#85c9b9"
+                      width="30"
+                    />
+                  ) : (
+                    <Icon
+                      icon="ant-design:dislike-filled"
+                      color="#f54a4a"
+                      width="30"
+                    />
+                  )}{" "}
+                </div>
+
+                <div className={style.ReviewRating}>
+                  <div className={style.Positive}>
+                    <Icon
+                      icon="ant-design:like-filled"
+                      color="rgba(18, 44, 52, 0.5)"
+                      width="20"
+                      height="20"
+                    />
+                    <p className={style.PositiveNumber}>{review.upvoteCount}</p>
+                  </div>
+                  <div className={style.Negative}>
+                    <Icon
+                      icon="ant-design:dislike-filled"
+                      color="rgba(18, 44, 52, 0.5)"
+                      width="20"
+                      height="20"
+                    />
+                    <p className={style.NegativeNumber}>
+                      {review.downvoteCount}
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className={style.Negative}>
-                <Icon
-                  icon="ant-design:dislike-filled"
-                  color="rgba(18, 44, 52, 0.5)"
-                  width="20"
-                  height="20"
-                />
-                <p className={style.NegativeNumber}>1</p>
-              </div>
+              {review.comment === null ? (
+                <p>Brak opini</p>
+              ) : (
+                <p className={style.OpinionText}>{review.comment}</p>
+              )}
             </div>
-          </div>
-          <p className={style.OpinionText}>
-            Aliquam at mattis nisl, ac fermentum magna. Mauris nec dignissim
-            justo. Ut id eleifend neque. Pellentesque eleifend ante non auctor
-            euismod.
-          </p>
-        </div>
+          ))
+        ) : (
+          <p></p>
+        )}
       </div>
     </div>
   );
