f08f494592df205568050fdeeb2d02b4983c2068
Moonikaa123 <98284081+Moonikaa123@users.noreply.github.com>
2023-01-08 14:11:35 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {createRoot} from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import { AuthContextProvider } from './context/AuthContext'
+import { AuthContextProvider } from './context/AuthContext';
+import { EventsContextProvider } from './context/EventsContext';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
    <AuthContextProvider>
-      <App/>;
+      <EventsContextProvider>
+         <App />;
+      </EventsContextProvider>
    </AuthContextProvider>
-)
+);
