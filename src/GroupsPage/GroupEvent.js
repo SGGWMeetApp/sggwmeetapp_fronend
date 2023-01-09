a45ecea0873f03a63254191e0b6e62ce31967c55
mavlevich <evgenij.mavlevich@gmail.com>
2023-01-09 01:11:52 +0100

    event group delete

--- src/GroupsPage/GroupEvent.js
+++ src/GroupsPage/GroupEvent.js
@@ -1,9 +1,12 @@
 import React from "react";
 import style from "./GroupsPage.module.css";
 import { Icon } from "@iconify/react";
-import TableRow from "./TableRow";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import moment from 'moment';
+import EventsTableRow from "../EventsPage/components/EventsTableRow";
+import DeleteModal from "../EventsPage/components/DeleteModal";
+
 class GroupEvents extends React.Component {
 
   constructor(props) {
@@ -15,26 +18,22 @@ class GroupEvents extends React.Component {
       id: undefined,
       openModal: false,
       userToken: user.token,
-      events: []
+      events: [],
+      displayEventMenu: false,
+      displayDeleteModal: false
     };
   }
 
-
-
-  componentDidMount() {
+  async componentDidMount() {
     this.setState({ id: +window.location.pathname.split("/")[3] })
-    this.getEvents(+window.location.pathname.split("/")[3]);
+    await this.getEvents(+window.location.pathname.split("/")[3]);
   }
 
-  OpenModal = () => {
+  setDisplayDeleteModal = (value) => {
     this.setState({
-      openModal: !this.state.openModal,
+      displayDeleteModal: value
     });
-  };
-  CloseModal = () => {
-    this.setState({
-      openModal: false,
-    });
+    this.getEvents(+window.location.pathname.split("/")[3]);
   };
 
   getEvents = async (id) => {
@@ -46,59 +45,58 @@ class GroupEvents extends React.Component {
     if (response.data.events.length > 0) {
       this.setState({ events: response.data.events, name: response.data.name });
     }
-
   };
 
   render() {
     return (
-      <div className={style.GroupContainer}>
-        <div className={style.GroupsSection}>
-          <div className={style.GroupsHeader}>
-            <h2 className={style.Header}>{this.state.name}</h2>
-            <div>
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? `${style.active}` : `${style.inactive}`
-                }
-                to={`/profile/groups/${this.state.id}/members`}
-              >
-                Członkowie
-              </NavLink>
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? `${style.active}` : `${style.inactive}`
-                }
-                to={`/profile/groups/${this.state.id}/events`}
-              >
-                Wydarzenia
-              </NavLink>
-            </div>
-            <div className={style.BtnContainer}>
-              <button className={style.BackMapBtn}>
-                <NavLink className={style.NavLinkBtn} to="/profile/groups">
-                  <Icon
-                    icon="akar-icons:arrow-left"
-                    color="#122c34"
-                    width="20"
-                    height="20"
-                  />
-                  Wróć do grup
+        <div className={style.GroupContainer}>
+          <div className={style.GroupsSection}>
+            <div className={style.GroupsHeader}>
+              <h2 className={style.Header}>{this.state.name}</h2>
+              <div>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? `${style.active}` : `${style.inactive}`
+                    }
+                    to={`/profile/groups/${this.state.id}/members`}
+                >
+                  Członkowie
                 </NavLink>
-              </button>
-              <button className={style.CreateGroupBtn}>
-                <Icon
-                  icon="ant-design:plus-outlined"
-                  color="white"
-                  width="20"
-                  height="20"
-                />
-                Dodaj wydarzenie
-              </button>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? `${style.active}` : `${style.inactive}`
+                    }
+                    to={`/profile/groups/${this.state.id}/events`}
+                >
+                  Wydarzenia
+                </NavLink>
+              </div>
+              <div className={style.BtnContainer}>
+                <button className={style.BackMapBtn}>
+                  <NavLink className={style.NavLinkBtn} to="/profile/groups">
+                    <Icon
+                        icon="akar-icons:arrow-left"
+                        color="#122c34"
+                        width="20"
+                        height="20"
+                    />
+                    Wróć do grup
+                  </NavLink>
+                </button>
+                <button className={style.CreateGroupBtn}>
+                  <Icon
+                      icon="ant-design:plus-outlined"
+                      color="white"
+                      width="20"
+                      height="20"
+                  />
+                  Dodaj wydarzenie
+                </button>
+              </div>
             </div>
-          </div>
-          <div>
-            <table className={style.GroupsTable}>
-              <thead>
+            <div>
+              <table className={style.GroupsTable}>
+                <thead>
                 <tr>
                   <th>Nazwa</th>
                   <th>Data</th>
@@ -106,35 +104,35 @@ class GroupEvents extends React.Component {
                   <th>Powiadomienie 24h przed</th>
                   <th></th>
                 </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <input type="checkbox" /> Spotkanie integracyjne
-                  </td>
-                  <td>16.12.2022 19:30</td>
-                  <td>Restauracja NoVa</td>
-                  <td>
-                    <input type="checkbox" />
-                  </td>
-                  <TableRow id="events" />
-                </tr>
-                <tr>
-                  <td>
-                    <input type="checkbox" /> Wyjście do Kina
-                  </td>
-                  <td>26.11.2022 18:45</td>
-                  <td>Kino Helios</td>
-                  <td>
-                    <input type="checkbox" />
-                  </td>
-                  <TableRow id="events" />
-                </tr>
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                {this.state.events.map(event => {
+                  return(
+                      <tr key={event.id}>
+                        <td>
+                          <input type="checkbox" /> {event.name}
+                        </td>
+                        <td>{moment(event.startDate).format('D.M.YYYY H:mm')}</td>
+                        <td>{event.locationData.name}</td>
+                        <td>
+                          <input type="checkbox" />
+                        </td>
+                        <EventsTableRow
+                            eventId={event.id}
+                            eventData={event}
+                            setDisplayDeleteModal={this.setDisplayDeleteModal}
+                        />
+                      </tr>
+                  )
+                })}
+                </tbody>
+              </table>
+            </div>
           </div>
+          {this.state.displayDeleteModal && (
+              <DeleteModal setDisplayDeleteModal={this.setDisplayDeleteModal} />
+          )}
         </div>
-      </div>
     );
   }
 }
